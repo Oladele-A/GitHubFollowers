da3ca802ed5e4e50a1ee7c062c4a952636ad2dd9
Oladele_Abimbola <zacchaeusola@outlook.com>
2022-09-09 13:46:10 +0100

    GFAlertVC Refactor

--- GitHubFollowers/Custom Views/ViewControllers/GFAlertViewController.swift
+++ GitHubFollowers/Custom Views/ViewControllers/GFAlertViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class GFAlertViewController: UIViewController {
     
-    let containerView = UIView()
+    let containerView = GFAlertContainerView()
     let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
     let messageLabel = GFBodyLabel(textAlignment: .center)
     let actionButton = GFButton(backgroundColor: .systemPink, title: "Ok")
@@ -42,11 +42,6 @@ class GFAlertViewController: UIViewController {
     
     func configureContainerView(){
         view.addSubview(containerView)
-        containerView.backgroundColor = .systemBackground
-        containerView.layer.cornerRadius = 16
-        containerView.layer.borderWidth = 2
-        containerView.layer.borderColor = UIColor.white.cgColor
-        containerView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
---
+++ GitHubFollowers/Custom Views/Views/GFAlertContainerView.swift
@@ -0,0 +1,28 @@
+//
+//  GFAlertContainerView.swift
+//  GitHubFollowers
+//
+//  Created by Oladele Abimbola on 9/9/22.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure(){
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
